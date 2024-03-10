0f26b9171f3fa0b55aeeb7908498bd4f50725f9c
J0w33l <J0w33l.D3v@gmail.com>
2024-03-10 13:35:37 -0600

    v2.1

--- js/Registro.js
+++ js/Registro.js
@@ -17,7 +17,7 @@ function showBTN() {
 
     Swal.fire({
         title: "Woof!",
-        text: "Debes enviarnos una foto de ti! Ya despues verás el porque!",
+        text: "Debes enviarnos una foto de tí, ya después verás el motivo!",
         icon: "Aceptar",
         confirmButtonColor: '#28a745'
     });
